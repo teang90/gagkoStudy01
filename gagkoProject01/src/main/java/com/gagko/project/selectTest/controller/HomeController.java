3863695a063fa01f3d2bb1a38e13a7d89a54a423
Your Name <minughe@gmail.com>
2019-08-26 21:28:46 +0900

    Hongju-

--- gagkoProject01/src/main/java/com/gagko/project/selectTest/controller/HomeController.java
+++ gagkoProject01/src/main/java/com/gagko/project/selectTest/controller/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		//
+		//Hongju
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
